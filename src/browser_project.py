a8a40b2cdbb25d14177b18233665020cc0e34e42
David Garcia <davidgmorillop@gmail.com>
2019-06-15 20:41:53 +0200

    init commit

---
+++ src/browser_project.py
@@ -0,0 +1,344 @@
+﻿"""
+Created on 20 ene. 2019
+
+@author: David
+"""
+import csv
+import datetime as dt
+from collections import namedtuple
+from statistics import mean
+from typing import List, Optional, Tuple
+
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+pd.plotting.register_matplotlib_converters()
+
+plt.style.use("bmh")
+
+
+def namedtuple_fixed(name: str, fields: list) -> namedtuple:
+    """Checks the fields of the namedtuple and changes the invalid ones."""
+
+    def starts_with_number(string: str) -> bool:
+        if string[0].isdigit():
+            return True
+        return False
+
+    fields_fixed: list = [
+        field.replace(" ", "_")
+        if not starts_with_number(field)
+        else f"c{field.replace(' ', '_')}"
+        for field in fields
+    ]
+
+    return namedtuple(name, fields_fixed)
+
+
+Records: namedtuple = None
+
+
+def read_file(file: str) -> List["Records"]:
+    """
+    Read the file with info about the percentage of usage of the browsers around the world.
+        INPUT = The file with the data
+        OUTPUT = List of tuples with the info of the csv file
+    """
+    global Records
+
+    with open(file, encoding="UTF-8") as browsers_file:
+        reader = csv.reader(browsers_file)
+        field_names = next(reader)
+        Records = namedtuple_fixed("Record", field_names)
+        result: List[Records] = [
+            Records(
+                *[
+                    dt.datetime.strptime(n, "%Y-%m").date()
+                    if record.index(n) == 0
+                    else float(n)
+                    for n in record
+                ]
+            )
+            for record in reader
+        ]
+    return result
+
+
+# def filter_by_importance(data, Filter):
+#     '''
+#         INPUT = Receive a data with a List of tuples with the percentage
+#         of usage of certain browser on certain date and a filter, which
+#         determines the minimum percentage needed to be on the list
+#         OUTPUT = Returns only the browsers with more percentage of usage than the filter
+#     '''
+#     result = []
+#     for Tuple in data:
+#         res = []
+#         for n in Tuple:
+#             if type(n)==float:
+#                 if n>=Filter:
+#                     res.append(n)
+#             else:
+#                 res.append(n)
+#         result.append(res)
+#     return result
+
+
+def filter_by_date(
+    data: List[Records], initial_date: str, final_date: str
+) -> List[Records]:
+    """
+        INPUT = data: list of tuples with the percentage of use
+        of certain browsers, on a certain date
+        initial_date: Initial date
+        final_date: Final date
+        OUTPUT = LIst of tuples with the browsers between these dates
+    """
+    result: List[Records]
+
+    if initial_date is None is final_date:
+        result = data
+
+    elif initial_date is None:
+        result = [
+            n
+            for n in data
+            if n.Date <= dt.datetime.strptime(final_date, "%Y-%m").date()
+        ]
+
+    elif final_date is None:
+        result = [
+            n
+            for n in data
+            if dt.datetime.strptime(initial_date, "%Y-%m").date() <= n.Date
+        ]
+
+    else:
+        result = [
+            n
+            for n in data
+            if dt.datetime.strptime(initial_date, "%Y-%m").date()
+            <= n.Date
+            <= dt.datetime.strptime(final_date, "%Y-%m").date()
+        ]
+
+    return result
+
+
+def filter_by_date_and_browser(
+    data: List[Records], date: str, browser: str
+) -> Optional[float]:
+    """
+    INPUT =
+    OUTPUT =
+    """
+    for record in data:
+        if dt.datetime.strptime(date, "%Y-%m").date() == record.Date:
+            return getattr(record, browser)
+
+    return None
+
+
+def filter_by_browser(data: List[Records], browser: str) -> List[Tuple[dt.date, float]]:
+    """
+    INPUT = data: list of tuples with the percentage of use
+    of certain browsers, on a certain date
+    Browser: the name of a browser
+    OUTPUT = a list of tuples with the percentage of use of the browser and the dates
+    """
+
+    return [(record.Date, getattr(record, browser)) for record in data]
+
+
+def plot_graph_browser(data: List[Records], list_of_browsers: List[str]) -> None:
+    """
+    INPUT =
+    OUTPUT =
+    """
+    records_by_browser: List[List[Tuple[dt.date, float]]] = [
+        filter_by_browser(data, browser) for browser in list_of_browsers
+    ]
+    percentages: List[List[float]] = [
+        [n[1] for n in record] for record in records_by_browser
+    ]
+    dates: List[List[dt.date]] = [
+        [n[0] for n in record] for record in records_by_browser
+    ]
+
+    for i, browser in enumerate(list_of_browsers):
+        plt.plot(
+            dates[i],
+            percentages[i],
+            label=browser,
+            marker="o",
+            markerfacecolor="purple",
+            markersize=2,
+        )
+    plt.xticks(rotation=30)
+    plt.legend()
+    plt.ylabel("Percentages")
+    plt.xlabel("Dates")
+    plt.title("Percentage of use")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_stick_graph(data: List[Records], list_of_browsers: List[str]) -> None:
+    """
+    INPUT =
+    OUTPUT =
+    """
+    means_of_usage: List[float] = [
+        mean([getattr(record, browser) for record in data])
+        for browser in list_of_browsers
+    ]
+
+    sns.barplot(means_of_usage, list_of_browsers, palette="rocket", orient="h")
+    plt.tight_layout()
+    plt.show()
+
+
+def filter_dataframe_by_list_of_browsers(
+    file: str, list_of_browsers: List[str]
+) -> pd.DataFrame:
+    """
+    INPUT =
+    OUTPUT =
+    """
+    list_of_browsers.append("Date")
+
+    data_frame: pd.DataFrame = pd.read_csv(
+        file, index_col="Date", usecols=list_of_browsers
+    )
+
+    outside: List[str] = []
+    records: List[float] = []
+
+    for date in list(data_frame.index):
+        outside.extend([date] * len(data_frame.columns))
+        records.extend(list(data_frame.loc[date]))
+
+    inside: List[str] = list(data_frame.columns) * len(data_frame)
+
+    assert len(inside) == len(outside)
+
+    hier_index: pd.MultiIndex = pd.MultiIndex.from_tuples(
+        zip(outside, inside), names=["Dates", "Browsers"]
+    )
+
+    data_frame_with_indexes: pd.DataFrame = pd.DataFrame(
+        data=records, index=hier_index, columns=["Records"]
+    )
+
+    return data_frame_with_indexes
+
+    # foo = np.array([filter_by_browser(data, Browser)
+    #                 for Browser in list_of_browsers]).reshape(120, 2)
+    # bar = pd.DataFrame(foo, columns=['Dates', 'Records'])
+    # list_ = []
+    # for lev in list(bar['Dates']):
+    #     list_.append(str(lev)[:7])
+    # quax = int(len(bar['Dates'])/len(list_of_browsers))
+    # bar['Dates'] = list_
+    # bar['Browsers'] = list_of_browsers * quax
+    # foobar = list(
+    #     zip(np.array(bar['Dates'].sort_values()), np.array(bar['Browsers'])))
+    # qoo = pd.MultiIndex.from_tuples(foobar)
+    # eggs = pd.DataFrame(bar, index=qoo)
+    # del eggs['Dates']
+    # del eggs['Browsers']
+    # potato = flattener([[filter_by_date_and_browser(data, foo, browser)
+    #                      for browser in list_of_browsers] for foo in eggs.index.levels[0]])
+    # eggs['Records'] = potato
+    # eggs.index.names = ['Dates', 'Browsers']
+    # return eggs
+
+
+def filter_dataframe_by_importance(file: str, filter_: float = 0.0) -> pd.DataFrame:
+    """
+    INPUT =
+    OUTPUT =
+    """
+    data_frame: pd.DataFrame = pd.read_csv(file, index_col="Date")
+    data_frame_filtered = data_frame[data_frame.columns[data_frame.mean() > filter_]]
+    return data_frame_filtered.transpose()
+
+
+def plot_evolution_browsers(file: str, list_of_browsers: List[str]) -> None:
+    """
+    TODO
+    """
+    list_of_browsers.append("Date")
+
+    data_frame: pd.DataFrame = pd.read_csv(
+        file, index_col="Date", usecols=list_of_browsers, parse_dates=True
+    )
+
+    data_frame.plot(
+        legend=True,
+        grid=True,
+        colormap="inferno",
+        title="Evolution of browser/s usage",
+        marker="o",
+        markerfacecolor="red",
+        markersize=2,
+    )
+    for browser in data_frame:
+
+        plt.fill_between(
+            data_frame.index, data_frame[browser], alpha=0.3, interpolate=True
+        )
+
+    plt.xlabel("Dates")
+    plt.ylabel("Percentage of use")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def filter_by_date_and_browser_with_dataframes(
+    file: str, date: str, browser: str
+) -> float:
+    """
+    TODO
+    """
+    data_frame: pd.DataFrame = pd.read_csv(file, index_col="Date")
+
+    return data_frame[browser].loc[date]
+
+
+def plot_pie_chart(
+    file: str, date: Optional[str] = None, *, circle: bool = False
+) -> None:
+    """
+    TODO
+    """
+    data_frame: pd.DataFrame = pd.read_csv(file, index_col="Date")
+
+    data: pd.Series = data_frame.mean() if date is None else data_frame.loc[date]
+    data_filtered: pd.Series = data[data > 1.0]
+
+    data_filtered["Other"] = 100 - sum(data_filtered)
+    data_filtered.sort_values(ascending=False, inplace=True)
+
+    explodes: tuple = (0.1,) + (0.0,) * (len(data_filtered) - 1)
+    plt.pie(
+        data_filtered,
+        labels=data_filtered.index,
+        explode=explodes,
+        shadow=True,
+        autopct="%1.2f%%",
+        pctdistance=0.85,
+    )
+
+    if circle:
+        centre_circle = plt.Circle((0, 0), 0.70, fc="white")
+        # fig = plt.gcf()
+        axis = plt.gca()
+
+        axis.add_artist(centre_circle)
+
+    plt.tight_layout()
+    plt.show()
---
+++ src/test_browser_project.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20 ene. 2019
+
+@author: David
+"""
+from datetime import datetime as dt
+from pprint import pprint
+from typing import List, Tuple
+
+import pandas as pd
+
+import browser_project
+
+
+def test_filter_by_date(data) -> None:
+    init_date = "2017-09"
+    final_date = "2018-12"
+    result: browser_project.Records = browser_project.filter_by_date(
+        data, init_date, final_date
+    )
+    if init_date is None is final_date:
+        print("Printing all records")
+    elif final_date is None:
+        print("Printing records since " + init_date)
+    elif init_date is None:
+        print("Printing records before " + final_date)
+    else:
+        print("Browser records between " + init_date + " and " + final_date + ":")
+    pprint(result)
+    print()
+
+
+def test_filter_by_date_and_browser(data) -> None:
+    date = "2018-07"
+    browser = "Safari"
+    percentage: float = browser_project.filter_by_date_and_browser(data, date, browser)
+    print(
+        f"The percentage of usage of the browser {browser} in {date} is {percentage}\n"
+    )
+
+
+def test_filter_by_browser(data) -> None:
+    browser = "Chrome"
+    result: List[Tuple[dt.date, float]] = browser_project.filter_by_browser(
+        data, browser
+    )
+    print(f"Printing the percentages of use of the browser {browser}:")
+    pprint(result)
+    print()
+
+
+def test_filter_data_frame_by_list_of_browsers(
+    file: str, list_of_browser: list, display_all: bool = False
+) -> None:
+    data_frame: pd.DataFrame = browser_project.filter_dataframe_by_list_of_browsers(
+        file, list_of_browser
+    )
+    if display_all:
+        with pd.option_context("display.max_rows", None, "display.max_columns", None):
+            print(data_frame, "\n")
+    else:
+        print(data_frame, "\n")
+
+
+def test_filter_data_frame_by_importance(data, filter_) -> None:
+    data_frame: pd.DataFrame = browser_project.filter_dataframe_by_importance(
+        data, filter_
+    )
+    print(
+        f"Printing the DataFrame with the browsers which mean is greater than {filter_}"
+    )
+    print(data_frame)
+
+
+def test_filter_by_date_and_browser_with_data_frame(file, date, browser) -> None:
+    print(f"The percentage of use of the browser {browser} on the date {date} is:")
+    print(
+        browser_project.filter_by_date_and_browser_with_dataframes(file, date, browser)
+    )
+
+
+if __name__ == "__main__":
+    # ===========================================================================
+    FILE_2009_to_2019 = "./data/browser-ww-monthly-200901-201902.csv"
+    FILE_2017_to_2018 = "./data/browser-ww-monthly-201701-201812.csv"
+
+    records: List[browser_project.Records] = browser_project.read_file(
+        FILE_2017_to_2018
+    )
+
+    pprint(records[:5])
+    print()
+
+    test_filter_by_date(records)
+
+    test_filter_by_date_and_browser(records)
+
+    test_filter_by_browser(records)
+
+    browser_project.plot_graph_browser(
+        records, ["Chrome", "Firefox", "Edge", "Safari", "IE"]
+    )
+
+    browser_project.plot_stick_graph(
+        records, ["Chrome", "Firefox", "Edge", "Safari", "IE"]
+    )
+
+    test_filter_data_frame_by_list_of_browsers(
+        FILE_2009_to_2019, ["Chrome", "Firefox", "Edge", "Safari", "IE"]
+    )
+
+    test_filter_data_frame_by_importance(FILE_2009_to_2019, 5.0)
+
+    browser_project.plot_evolution_browsers(
+        FILE_2009_to_2019, ["Chrome", "Firefox", "Edge", "Safari", "IE"]
+    )
+
+    test_filter_by_date_and_browser_with_data_frame(
+        FILE_2017_to_2018, "2017-02", "Firefox"
+    )
+
+    browser_project.plot_pie_chart(FILE_2017_to_2018, "2017-02", circle=True)
+    # ===========================================================================
